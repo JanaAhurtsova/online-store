d5121e02d735571a9e2d962a66892b5c150a1bbb
Jana Ahurtsova <byjankaaa@gmail.com>
2022-12-30 19:54:16 +0300

    refactor: delete empty brackets

--- src/components/view/shoppingCart/modal/personalInfo/personalInfo.ts
+++ src/components/view/shoppingCart/modal/personalInfo/personalInfo.ts
@@ -52,22 +52,21 @@ export default class PersonalInfo {
     this.personalInfoTitle.classList.add('personal_information__title');
     this.personalInfoTitle.textContent = 'Personal Information';
 
-    this.fullName = this.createInputNode(this.fullName, 'Full Name', '', 'input', 'full_name');
+    this.fullName = this.createInputNode(this.fullName, 'Full Name', 'input', 'full_name');
     this.fullNameContainer = this.appendInput(this.fullNameContainer, 'input__field', this.fullName, this.errorName);
 
-    this.phone = this.createInputNode(this.phone, 'Phone Number', '', 'input', 'phone');
+    this.phone = this.createInputNode(this.phone, 'Phone Number', 'input', 'phone');
     this.phoneContainer = this.appendInput(this.phoneContainer, 'input__field', this.phone, this.errorPhone);
 
-    this.address = this.createInputNode(this.address, 'Delivery Address', '', 'input', 'address');
+    this.address = this.createInputNode(this.address, 'Delivery Address', 'input', 'address');
     this.addressContainer = this.appendInput(this.addressContainer, 'input__field', this.address, this.errorAddress);
 
-    this.email = this.createInputNode(this.email, 'E-mail', '', 'input', 'email');
+    this.email = this.createInputNode(this.email, 'E-mail', 'input', 'email');
     this.emailContainer = this.appendInput(this.emailContainer, 'input__field', this.email, this.errorEmail);
   }
 
-  private createInputNode(node: HTMLInputElement, placeholder: string, id: string, ...classes: string[]) {
+  private createInputNode(node: HTMLInputElement, placeholder: string, ...classes: string[]) {
     node.placeholder = placeholder;
-    node.id = id;
     node.classList.add(...classes);
     return node;
   }
